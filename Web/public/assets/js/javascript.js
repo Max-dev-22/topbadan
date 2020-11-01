829d6f17b90229705fb6f0be9fd00099c4513575
Max <maxdillondevwork@gmail.com>
2020-11-02 00:17:21 +0330

    topbadan

--- Web/public/assets/js/javascript.js
+++
@@ -1,7 +0,0 @@
-function MoveToOpen() {
-    document.getElementById("demo").style.height = "100px";
-  }
-function MoveToClose() {
-    document.getElementById("demo").style.height = "0px";
-  }
- 
